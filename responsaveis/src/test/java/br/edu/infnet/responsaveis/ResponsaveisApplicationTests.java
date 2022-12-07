28deae0ddc77cb811ae940710861f275dcd779ae
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2022-12-06 23:59:41 -0300

    Configuracoes iniciais

---
+++ responsaveis/src/test/java/br/edu/infnet/responsaveis/ResponsaveisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.responsaveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponsaveisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
